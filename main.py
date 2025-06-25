4ad100798ba655019f35189e3d9758d83746445d
Oleg Danilchenko <olegdanilchenko1@MacBook-Pro-2.local>
2025-06-25 21:29:24 +0300

    Встановлено Python 3.11 для сумісності3

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-import sys
-from fastapi import FastAPI
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from aiogram.dispatcher.webhook import SendMessage
+from fastapi import FastAPI, Request
+import os
 
+BOT_TOKEN = "7795558482:AAE8WEmzTJqQkfSLKUPXjVK40QIUC2mitYg"
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
 app = FastAPI()
 
-@app.get("/python-version")
-def get_python_version():
-    return {"python_version": sys.version}
+@dp.message_handler(commands=["start"])
+async def cmd_start(message: Message):
+    return SendMessage(message.chat.id, "Привіт! Я на webhook.")
+
+@app.post("/webhook")
+async def process_webhook(request: Request):
+    update = types.Update(**await request.json())
+    await dp.process_update(update)
+    return {"status": "ok"}
