8f4171257857cc56b5491d428266fca5346772bd
Oleg Danilchenko <olegdanilchenko1@MacBook-Pro-2.local>
2025-06-25 20:40:02 +0300

    Встановлено Python 3.11 для сумісності2

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
-from fastapi import FastAPI, Request
-import os
-import telegram
+import sys
+from fastapi import FastAPI
 
-# 🔑 Токен бота
-TOKEN ="7795558482:AAE8WEmzTJqQkfSLKUPXjVK40QIUC2mitYg"
-
-# 🤖 Ініціалізація бота
-bot = telegram.Bot(token=TOKEN)
-
-# 🌐 FastAPI додаток
 app = FastAPI()
 
-# 📩 Обробка вхідних POST-запитів з Telegram
-@app.post("/webhook")
-async def telegram_webhook(request: Request):
-    data = await request.json()
-    message = data.get("message", {})
-    chat_id = message.get("chat", {}).get("id")
-
-    if chat_id:
-        bot.send_message(chat_id=chat_id, text="Зрозумів")
-
-    return {"status": "ok"}
+@app.get("/python-version")
+def get_python_version():
+    return {"python_version": sys.version}
